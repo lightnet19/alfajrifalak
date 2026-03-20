6ad529670a698170bce6f31e05af8f76d1a04015
lightnet19 <light.net19@gmail.com>
2026-03-20 08:54:06 +0700

    Delete public directory

--- public/core/hijriDecision.js
+++
@@ -1,5 +0,0 @@
-export function isNewMonth({ ijtima, sunset, visible }) {
-  if (!ijtima || sunset == null) return false;
-  const ijtimaHour = ijtima.getHours() + ijtima.getMinutes() / 60 + ijtima.getSeconds() / 3600;
-  return ijtimaHour < sunset && visible;
-}
--- public/core/illumination.js
+++
@@ -1,5 +0,0 @@
-export function getIllumination(elongation) {
-  const rad = Math.PI / 180;
-  const illumination = (1 + Math.cos(elongation * rad)) / 2;
-  return { phaseAngle: elongation, illumination };
-}
