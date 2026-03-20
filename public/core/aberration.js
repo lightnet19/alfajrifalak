79b78663896e60bc2838debda7e7afb5ec81252a
lightnet19 <light.net19@gmail.com>
2026-03-20 08:28:11 +0700

    Delete public directory

--- public/core/aberration.js
+++
@@ -1,6 +0,0 @@
-export function getAberration(lambda, T) {
-  const rad = Math.PI / 180;
-  const k = 20.49552 / 3600;
-  const M = 357.52911 + 35999.05029 * T;
-  return -k * Math.cos((lambda - M) * rad);
-}
--- public/core/irnu.js
+++
@@ -1,9 +0,0 @@
-export function getIRNUCriteria({ altitude, elongation }) {
-  const memenuhi = altitude >= 3 && elongation >= 6.4;
-  return {
-    altitude,
-    elongation,
-    memenuhi,
-    keterangan: memenuhi ? "Memenuhi Kriteria IRNU (NU)" : "Belum Memenuhi Kriteria IRNU",
-  };
-}
