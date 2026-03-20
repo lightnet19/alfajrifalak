c1dc6c2e8bfa1415c6d8da2426a62ad80df2ed5a
lightnet19 <light.net19@gmail.com>
2026-03-20 09:11:11 +0700

    Update hilal.js

--- public/js/engine/hilal.js
+++ public/js/engine/hilal.js
@@ -1,8 +1,10 @@
 import { toJulianDate, getDeltaT } from "./time.js";
 import { getSun } from "./sun.js";
 import { getMoon } from "./moon.js";
+import { getSiderealTime, equatorialToHorizontal } from "./coordinate.js";
+import { getSunset } from "./sunset.js";
 
-export function calculateHilal(date) {
+export function calculateHilal(date, lat, lon, tz) {
   const JD = toJulianDate(date);
   const deltaT = getDeltaT(date.getFullYear());
 
@@ -12,16 +14,34 @@ export function calculateHilal(date) {
   const sun = getSun(T);
   const moon = getMoon(T);
 
+  const LST = getSiderealTime(JD, lon);
+
+  const sunHor = equatorialToHorizontal(
+    sun.RA,
+    sun.Dec,
+    lat,
+    LST
+  );
+
+  const moonHor = equatorialToHorizontal(
+    moon.RA,
+    moon.Dec,
+    lat,
+    LST
+  );
+
   const elongation = Math.abs(moon.lambda - sun.lambda);
 
-  const illumination =
-    (1 - Math.cos(elongation * Math.PI / 180)) / 2;
+  const sunset = getSunset(date, lat, lon, tz);
 
   return {
     JD,
     deltaT,
+    sunset,
+    sunAltitude: sunHor.alt,
+    moonAltitude: moonHor.alt,
+    sunAzimuth: sunHor.az,
+    moonAzimuth: moonHor.az,
     elongation,
-    illumination,
-    distance: moon.distance,
   };
 }
