fab2c56c3b6706e9273938091ef72ab6867faa9e
lightnet19 <light.net19@gmail.com>
2026-03-20 09:09:52 +0700

    Update sun.js

--- public/js/engine/sun.js
+++ public/js/engine/sun.js
@@ -1,16 +1,31 @@
-import { normalize } from "./utils.js";
-
 export function getSun(T) {
   const rad = Math.PI / 180;
 
-  const L0 = 280.466 + 36000.77 * T;
+  const L = 280.466 + 36000.77 * T;
   const M = 357.529 + 35999.05 * T;
 
   const C =
     1.914 * Math.sin(M * rad) +
     0.020 * Math.sin(2 * M * rad);
 
-  const lambda = normalize(L0 + C);
+  const lambda = (L + C) % 360;
 
-  return { lambda };
+  const epsilon = 23.439;
+
+  const RA =
+    Math.atan2(
+      Math.cos(epsilon * rad) * Math.sin(lambda * rad),
+      Math.cos(lambda * rad)
+    ) / rad;
+
+  const Dec =
+    Math.asin(
+      Math.sin(epsilon * rad) * Math.sin(lambda * rad)
+    ) / rad;
+
+  return {
+    lambda,
+    RA: (RA + 360) % 360,
+    Dec,
+  };
 }
